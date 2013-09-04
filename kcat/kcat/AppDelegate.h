b1268f5e41833f73aad653249d52fa6d9b719317
modivinit <modivinit@gmail.com>
2013-09-04 16:28:24 -0700

    MOre Vinit's changes

---
+++ kcat/kcat/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  kcat
+//
+//  Created by Vinit Modi on 8/28/13.
+//  Copyright (c) 2013 Vinit Modi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ kcat/kcat/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  kcat
+//
+//  Created by Vinit Modi on 8/28/13.
+//  Copyright (c) 2013 Vinit Modi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
