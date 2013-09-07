d37a74a8ff55d433a9e6ff68d63a282277714478
modivinit <modivinit@gmail.com>
2013-09-07 00:04:17 -0700

    New UI
    Major UI overhaul with smaller icons and larger space for text. Also
    includes new buttons with arrows.

---
+++ kcat/kcat/aboutyou.h
@@ -0,0 +1,13 @@
+//
+//  aboutyou.h
+//  kcat
+//
+//  Created by Vinit Modi on 9/6/13.
+//  Copyright (c) 2013 Vinit Modi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface aboutyou : UIViewController
+
+@end
---
+++ kcat/kcat/aboutyou.m
@@ -0,0 +1,38 @@
+//
+//  aboutyou.m
+//  kcat
+//
+//  Created by Vinit Modi on 9/6/13.
+//  Copyright (c) 2013 Vinit Modi. All rights reserved.
+//
+
+#import "aboutyou.h"
+
+@interface aboutyou ()
+
+@end
+
+@implementation aboutyou
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
