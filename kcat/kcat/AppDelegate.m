93a5943b1a4a9ff34c4339a52bc4712c2dc55eda
modivinit <modivinit@gmail.com>
2013-09-23 23:13:21 -0700

    updated intro screen

--- kcat/kcat/AppDelegate.m
+++ kcat/kcat/AppDelegate.m
@@ -18,7 +18,7 @@
     self.window.backgroundColor = [UIColor whiteColor];
     UIViewController* controller = [[UIViewController alloc] init];
 
-    UIView *firstViewUIView = [[[NSBundle mainBundle] loadNibNamed:@"dashboard-noinfo"
+    UIView *firstViewUIView = [[[NSBundle mainBundle] loadNibNamed:@"kcatintro"
                                                              owner:self options:nil] objectAtIndex:0];
     [controller.view addSubview:firstViewUIView];
 
