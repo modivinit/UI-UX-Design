0717696f2c06e9f2d5638cbf4176107b754fcc53
modivinit <vinitmodi@Vinits-MacBook-Pro.local>
2013-10-07 10:51:28 -0700

    Created new images for the menu.

--- kcat/kcat/AppDelegate.m
+++ kcat/kcat/AppDelegate.m
@@ -18,7 +18,7 @@
     self.window.backgroundColor = [UIColor whiteColor];
     UIViewController* controller = [[UIViewController alloc] init];
 
-    UIView *firstViewUIView = [[[NSBundle mainBundle] loadNibNamed:@"homeaddress"
+    UIView *firstViewUIView = [[[NSBundle mainBundle] loadNibNamed:@"homeinfo"
                                                              owner:self options:nil] objectAtIndex:0];
     [controller.view addSubview:firstViewUIView];
 
